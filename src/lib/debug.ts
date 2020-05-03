69770c767b2ed3177bed36f346f7d6df8e1640c5
Juro Oravec <juraj.oravec.josefson@gmail.com>
2020-05-03 10:09:17 +0200

    refactor(logging): rename debug export from mainDebug to debug

--- src/lib/debug.ts
+++ src/lib/debug.ts
@@ -1,4 +1,4 @@
-import debug from 'debug';
+import debugPkg from 'debug';
 import readPkgUp from 'read-pkg-up';
 
 function getDebugLogger() {
@@ -10,9 +10,9 @@ function getDebugLogger() {
     );
     return console.log;
   }
-  return debug(name);
+  return debugPkg(name);
 }
 
-const mainDebug = getDebugLogger();
+const debug = getDebugLogger();
 
-export default mainDebug;
+export default debug;
